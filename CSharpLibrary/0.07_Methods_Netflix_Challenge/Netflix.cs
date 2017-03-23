9470347dc55e7018d6b15770f3a2b8b6bd047085
Adam Demaree <demareeadam1@gmail.com>
2017-03-23 10:37:53 -0400

    Methods Challege Day 2

---
+++ CSharpLibrary/0.07_Methods_Netflix_Challenge/Netflix.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._07_Methods_Netflix_Challenge
+{
+    class Netflix
+    {
+        
+
+        //Properties
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+
+
+        //Methods
+        public void GetSuggestions()
+        {
+            if (this.Rating >= 4)
+                Console.WriteLine("You've gotta see this show!");
+            else
+                Console.WriteLine("Don't waste your life with this thing.");
+        }
+
+
+
+
+        public string PayBill()
+        {
+            return "You just paid your bill";
+        }
+          
+    }
+}
---
+++ CSharpLibrary/0.07_Methods_Netflix_Challenge/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._07_Methods_Netflix_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Netflix rulesofengagement = new Netflix();
+            rulesofengagement.Name = "Rules of Engagement";
+            rulesofengagement.Genre = "Comedy";
+            rulesofengagement.Rating = 4.5;
+
+            Netflix friends = new Netflix();
+            friends.Name = "Friends";
+            friends.Genre = "Romantic Comedy";
+            friends.Rating = 3.8;
+
+            Netflix daredevil = new Netflix();
+            daredevil.Name = "DareDevil";
+            daredevil.Genre = "Action";
+            daredevil.Rating = 4.3;
+
+            rulesofengagement.GetSuggestions();
+            friends.GetSuggestions();
+            daredevil.GetSuggestions();
+            Console.ReadLine();
+        }
+            
+        
+    }
+}
