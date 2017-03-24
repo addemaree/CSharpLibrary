337243f499c16ef12245ff28854d17be9dedf2d4
Adam Demaree <demareeadam1@gmail.com>
2017-03-24 12:16:27 -0400

    Inheritance and Access_Modifiers

---
+++ CSharpLibrary/0.12_Inheritance/Dog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._12_Inheritance
+{
+    class Dog : Animal
+    {
+        public bool Bark { get; set; }
+        public int NumberOfPaws { get; set; }
+        public string Gender { get; set; }
+        public int NumberofPaws { get; internal set; }
+
+        public void Runs()
+        {
+            Console.WriteLine("I just ran a lap around the yard. Watch me do it again!");
+        }
+
+        public void Protect()
+        {
+            Console.WriteLine("BARK BARK (Stranger Danger!)");
+        }
+
+
+    }
+}
---
+++ CSharpLibrary/0.12_Inheritance/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._12_Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bear barryTheBear = new Bear();
+            barryTheBear.HasEyes = true;
+            barryTheBear.Hibernate = true;
+            barryTheBear.Meal = "Humans";
+            barryTheBear.NumberOfLegs = 4;
+            barryTheBear.Weight = 2000;
+
+            Console.WriteLine("Bear Methods");
+            barryTheBear.Walks();
+            barryTheBear.Speak();
+
+
+            Console.WriteLine("==============");
+
+            Console.WriteLine("Dog Methods");
+
+            Dog spotTheDog = new Dog();
+            spotTheDog.NumberOfPaws = 4;
+            spotTheDog.Gender = "Male";
+            spotTheDog.Bark = false;
+
+            
+            spotTheDog.Runs();          
+            spotTheDog.Protect();
+
+            Console.WriteLine("=========");
+
+            Console.WriteLine("Polar Bear Methods");
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
